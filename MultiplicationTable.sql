06aa379e69acf89436b40b4f278f3240a0851aa5
tirthb101 <bhatiatirth01@gmail.com>
2023-02-13 12:45:54 +0530

    made asssignment operator consistant

--- MultiplicationTable.sql
+++ MultiplicationTable.sql
@@ -2,7 +2,7 @@ declare
     number1 integer;
     i integer;
 begin
-    number1 = &number1;
+    number1 := &number1;
     for i in 1..10 loop
       dbms_output.put_line(number1 || " x " || i || " = "|| number1 * i);
     end loop
--- swap.sql
+++ swap.sql
@@ -3,8 +3,8 @@ declare
     b integer;
     temp integer;
 begin
-  a = &a;
-  b = &b;
+  a := &a;
+  b := &b;
   dbms_output.put_line("Before "||a || " | " ||b);
   temp:= b;
   b:= a;
